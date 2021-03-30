84d539f76ce29de2042d4b9aa2191d1ea526ce2f
Nerissa Lin <lin.nerissa@gmail.com>
2021-03-29 21:52:21 -0700

    cant quite figure out how to use ReactDom Render

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-async func callBackendAPI() {
+async function callBackendAPI() {
   const response = await fetch("/news");
   const body = await response.json();
 
@@ -19,8 +12,20 @@ async func callBackendAPI() {
     throw Error(body.message);
   }
   return body;
-};
+}
 
+// function replace
+
+callBackendAPI().then((body) =>
+  ReactDOM.render(body, document.getElementById("root"));
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
